ba2c91146fcbe8e2a4eb1a1e580a4071be68cb12
bam <juhyun19981129@gmail.com>
2022-04-26 22:07:27 +0900

    리덕스 상태관리를 위한 기초 작업 완료 지점

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,13 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
+import {createStore} from 'redux';
+import rootReducer from './modules';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {Provider} from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(rootReducer, composeWithDevTools());
 
 root.render(
-  <BrowserRouter>
-      <App/>
-  </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- front-end/src/modules/auth.js
+++ front-end/src/modules/auth.js
@@ -1,5 +1,10 @@
 /*
-*
+* auth(login, register)에 대해 리덕스 상태관리를 하기 위한 리덕스 모듈 파일입니다.
  */
-import React from 'react';
-impor
+import {createAction, handleActions} from 'redux-actions';
+
+const initialState = {};
+
+const auth = handleActions({}, initialState);
+
+export default auth;
