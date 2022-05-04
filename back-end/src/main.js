b68c16655214ece92b58c40e6610ca14235984da
bam <juhyun19981129@gmail.com>
2022-05-04 20:46:40 +0900

    api 연결 문제 해결

--- back-end/src/main.js
+++ back-end/src/main.js
@@ -9,6 +9,7 @@ import api from './api';
 import jwtMiddleware from './lib/jwtMiddleware';
 
 const {PORT, MONGO_URI} = process.env;
+const port = PORT || 4000;
 
 mongoose.connect(MONGO_URI)
     .then(() => console.log('Connected to MongoDB'))
@@ -17,13 +18,10 @@ mongoose.connect(MONGO_URI)
 const app = new Koa();
 const router = new Router();
 
-//TODO 도대체 어디에서 404 Not Found가 일어났는가...? 추정하기로는 api연결의 문제?
-router.use('./api', api.routes());
+router.use('/api', api.routes());
 
 app.use(bodyParser());
 app.use(jwtMiddleware);
 app.use(router.routes()).use(router.allowedMethods());
-
-const port = PORT || 4000;
 
 app.listen(port, () => console.log('%d번 포트에서 응답중...', port));
