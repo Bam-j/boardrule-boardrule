887de436e582ef0cd3bad6a4d726268f56ad2ccb
bam <juhyun19981129@gmail.com>
2022-05-29 22:34:18 +0900

    드디어 다음페이지 구현 반드시 이 이후로는 반드시 여기로 revert

--- front-end/src/components/game-rule-page/PageActionButtons.js
+++ front-end/src/components/game-rule-page/PageActionButtons.js
@@ -1,34 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
 import Button from '../common/Button';
-import { index } from './RulePage';
+import { increaseIndex, decreaseIndex } from './RulePage';
 import { useNavigate } from 'react-router-dom';
 
 const PageActionButtons = ({ isFirstPage, nextPageSrc }) => {
   const navigate = useNavigate();
 
-  const onPrevButtonClick = () => {
-    index -= 1;
-
+  const onClickPrevButton = e => {
+    e.preventDefault();
+    decreaseIndex();
     navigate(-1);
   };
 
-  const onNextButtonClick = () => {
-    index += 1;
+  const onClickNextButton = e => {
+    e.preventDefault();
+    increaseIndex();
   };
 
   return (
     <StyledPageActionButtonsBlock>
       {isFirstPage ? (
-        <Button className={'nextButton'} to={nextPageSrc} onClick={onNextButtonClick}>
+        <Button className={'nextButton'} to={nextPageSrc} onClick={onClickNextButton}>
           다음
         </Button>
       ) : (
         <div>
-          <Button className={'prevButton'} onClick={onPrevButtonClick}>
+          <Button className={'prevButton'} onClick={onClickPrevButton}>
             이전
           </Button>
-          <Button className={'nextButton'} to={nextPageSrc} onClick={onNextButtonClick}>
+          <Button className={'nextButton'} to={nextPageSrc} onClick={onClickNextButton}>
             다음
           </Button>
         </div>
