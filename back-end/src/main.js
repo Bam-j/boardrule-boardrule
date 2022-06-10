0eeb6e96eefa8a24959d3805f66d89594a65a184
bam <juhyun19981129@gmail.com>
2022-06-10 17:32:36 +0900

    서버 배포 준비

--- back-end/src/main.js
+++ back-end/src/main.js
@@ -4,6 +4,9 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
+import path from 'path';
+import serve from 'koa-static';
+import send from 'koa-send';
 
 import api from './api';
 import jwtMiddleware from './lib/jwtMiddleware';
@@ -23,5 +26,14 @@ router.use('/api', api.routes());
 app.use(bodyParser());
 app.use(jwtMiddleware);
 app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(__dirname, '../../front-end/build');
+
+app.use(serve(buildDirectory));
+app.use(async ctx => {
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    await send(ctx, 'index.html', { root: buildDirectory });
+  }
+});
 
 app.listen(port, () => console.log('%d번 포트에서 응답중...', port));
